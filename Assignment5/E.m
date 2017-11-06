ff8d54eb4e6e8d165ebe89bc98d99c130ae97d3c
Kyle Arens <arenskyle@gmail.com>
2017-11-05 20:08:56 -0500

    Update E.m

--- Assignment5/E.m
+++ Assignment5/E.m
@@ -1,14 +1,13 @@
-function [ sum ] = E( i, x, y )
+function [ sumVal ] = E( i, x, y, alpha, b )
 %UNTITLED5 Summary of this function goes here
 %   Detailed explanation goes here
 
-sum = 0;
+sumVal = 0;
 
-for j=1:l
-    sum = sum + (alpha(j) .* y(j) .* x(i) .* x(j) + b);
+for j=1:length(y)
+    sumVal = sumVal + (alpha(j) .* y(j) .* sum(x(i,:).*x(j,:)) + b);
 end
 
-sum = sum - y(i);
+sumVal = sumVal - y(i);
 
 end
-
