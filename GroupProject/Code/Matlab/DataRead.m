31b24363752db1ca332f8822e3a08d6aa77f7081
Kyle Arens <arenskyle@gmail.com>
2017-12-06 19:09:21 -0500

    added 'type' column

--- GroupProject/Code/Matlab/DataRead.m
+++ GroupProject/Code/Matlab/DataRead.m
@@ -6,12 +6,18 @@ folder = dir(textFolder);
 machineCodes = {'jmp', 'movl', 'pushl', 'jg', 'je', 'jle', 'xorl', 'cmpl', 'andl', 'subl', 'call', 'addl', 'leal'};
 
 for file = folder'
-    record = file.importdata(file);
+    record = file.importdata(file.name);
     codesRow = zeros(machineCodes.length());
     for i = 1:record.data.length()
         curIndex = find(contains(machineCodes, record.textdata(i)));
         codesRow(curIndex) = record.data(i);
     end
+    if contains(file.name, 'sort')
+        type = 1;
+    else
+        type = -1;
+    end
+    codesRow = [codesRow, type];
     codeRecords = [codeRecords; codesRow];
 end
 
